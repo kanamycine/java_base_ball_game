2122ea407f86d93d4dcd157e0c2f45188dfd46f9
kanamycine <jinyeong5320@gmail.com>
2021-09-09 22:13:22 +0900

    feat(baseball): 컴퓨터의 수와 플레이어의 수를 비교할 수 있는 기능 구현

--- src/main/java/baseballafterfeedback/Appplication.java
+++ src/main/java/baseballafterfeedback/Appplication.java
@@ -8,7 +8,7 @@ package baseballafterfeedback;
 
 // 기능 목록
 // - [x] 1부터 9까지의 서로 다른 임의의 수 3개를 생성한다. - NumberGenerator#createRandomNumber()
-// - [ ] 컴퓨터의 수 (3자리)와 플레이어의 수 (3자리)를 비교할 수 있다. - Referee#compare()
+// - [x] 컴퓨터의 수 (3자리)와 플레이어의 수 (3자리)를 비교할 수 있다. - Referee#compare()
 //      - [x] 몇 개의 숫자가 같은지를 알 수 있다. - Judgement#correctCount()
 //      - [x] 특정 자리에 특정 숫자가 있는 지 알 수 있다. - Judgement#hasPlace()
 // - [ ] 같은 수가 다른 자리에 있으면 볼이다. -
@@ -17,20 +17,15 @@ package baseballafterfeedback;
 
 import baseballafterfeedback.domain.Judgement;
 import baseballafterfeedback.domain.NumberGenerator;
+import baseballafterfeedback.domain.Referee;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Appplication {
     public static void main(String[] args) {
-//        NumberGenerator generator = new NumberGenerator();
-//        List<Integer> numbers = generator.createRandomNumbers();
-//        System.out.println(numbers);
-
-        Judgement judgement = new Judgement();
-//        int count = judgement.correctCount(c, Arrays.asList(1, 2, 3));
-//        System.out.println(count);
-        boolean place = judgement.hasPlace(Arrays.asList(2, 8, 9), 0, 2);
-        System.out.println(place);
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
+        System.out.println(result);
     }
 }
--- src/main/java/baseballafterfeedback/domain/Referee.java
+++ src/main/java/baseballafterfeedback/domain/Referee.java
@@ -4,6 +4,19 @@ import java.util.List;
 
 public class Referee {
     public String compare(List<Integer> computer, List<Integer> player) {
-        return "3 스트라이크";
+        // 몇개의 숫자가 포함되어 있는지 알아 낸뒤
+        // 스트라이크의 갯수를 구해서 뺀다
+        // 남은 수는 볼의 개수
+        Judgement judgement = new Judgement();
+        int correctCount = judgement.correctCount(computer, player);
+
+        int strike = 0;
+        for (int placeIndex = 0 ; placeIndex < player.size(); placeIndex++){
+            if (judgement.hasPlace(computer, placeIndex, player.get(placeIndex))){
+                strike++;
+            }
+        }
+        int ball = correctCount - strike;
+        return ball + "볼 " + strike + "스트라이크";
     }
 }
